3fda5d399c6478b8bb5414284b16d51f5010bbe2
BuildTools <unconfigured@null.spigotmc.org>
2019-12-24 17:00:19 +0800

    Improving CSS

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const PORT = 3000;
+
 const express = require('express');
 
 const app = express();
 app.use(express.static('docs'));
 
-app.listen(3000)
+app.listen(PORT, () => {
+    console.log('Server started on port: ' + PORT)
+})
